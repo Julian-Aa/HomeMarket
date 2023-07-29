58482e00e493d426afc5e670c16996b99b341bb9
LrdSanti <hernandez.santiago.5367@eam.edu.co>
2023-07-29 10:06:58 -0500

    Rama quienes somos con componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuienesSomosComponent
   ],
   imports: [
     BrowserModule,
